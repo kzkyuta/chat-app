b16c1c8b9027a9145adb3c02f8cd68e942ca3516
kzkyuta <kzkyuta@gmail.com>
2023-04-02 13:21:46 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "relative", height: "500px" }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "just now",
+                sender: "Joe",
+                direction: "incoming",
+                position: 0,
+              }}
+            />
+          </MessageList>
+          <MessageInput placeholder="Type message here" />
+        </ChatContainer>
+      </MainContainer>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
