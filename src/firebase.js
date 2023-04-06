82eca6fa9779f5ef4562b241bc10197bb8fe7f78
kzkyuta <kzkyuta@gmail.com>
2023-04-06 17:31:59 +0900

    modified firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// import firebase from 'firebase/app';
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-}
+};
 
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
+initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth();
